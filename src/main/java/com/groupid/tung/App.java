279f15c86f4cb83690bf7f539278b228d3bf420d
Tung Dao <tungdao17@gmail.com>
2023-08-02 10:27:45 +0800

    Successfully debug flink in vscode

--- src/main/java/com/groupid/tung/App.java
+++ src/main/java/com/groupid/tung/App.java
@@ -1,6 +1,12 @@
 package com.groupid.tung;
 
+import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.walkthrough.common.sink.AlertSink;
+import org.apache.flink.walkthrough.common.entity.Alert;
+import org.apache.flink.walkthrough.common.entity.Transaction;
+import org.apache.flink.walkthrough.common.source.TransactionSource;
+
 
 /**
  * Hello world!
@@ -8,8 +14,24 @@ import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
  */
 public class App
 {
-    public static void main( String[] args )
-    {
+    public static void main( String[] args ) throws Exception {
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+
+        DataStream<Transaction> transactions = env
+			.addSource(new TransactionSource())
+			.name("transactions");
+
+        DataStream<Alert> alerts = transactions
+			.keyBy(Transaction::getAccountId)
+			.process(new FraudDetector())
+			.name("fraud-detector");
+
+        alerts
+			.addSink(new AlertSink())
+			.name("send-alerts");
+
+        env.execute("Fraud Detection");
+
         System.out.println( "Hello World!" );
     }
 }
